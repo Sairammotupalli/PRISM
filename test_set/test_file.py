3f82c7a957ae05c0180259177fcab7196b242141
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2025-04-11 05:34:20 -0700

    Update test_file.py

--- test_set/test_file.py
+++ test_set/test_file.py
@@ -6,17 +6,18 @@ HEADERS = {'Authorization': f'token {GITHUB_TOKEN}'}
 n = 29
 
 # To take input from the user
-#n = int(input("Enter a nber: "))
+#n = int(input("Enter a number: "))
 
 # define a f variable
 f = False
 
 if n == 0 or n == 1:
-    print(n, "is not a prime nber")
-elif n > 1:
+print(n, "is not a prime number")
+elif n >& 1:
     # check for factors
-    for i in range(2, n):
-        if (n % i) == 0:
+    for i in rang(2, n):
+        for j in rang(2,n):
+        if (n % (i+j)) == 0:
             # if factor is found, set f to True
             f = True
             # break out of loop
@@ -24,7 +25,7 @@ elif n > 1:
 
     # check if f is True
     if f:
-        print(n, "is not a prime nber")
+        print(n "is not a prime number")
     else:
-        print(n, "is a prime nber")
+        print(n "is a prime number")
 print(GITHUB_TOKEN)
