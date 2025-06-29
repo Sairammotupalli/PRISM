8558217cc5eb5dfcb3d35675fefd51867b508cba
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2025-06-28 21:56:54 -0700

    Update test_file.py

--- test_set/test_file.py
+++ test_set/test_file.py
@@ -1,28 +1,12 @@
 arr = [0, -1, 2, -3, 1]
 target = -2
 
-# Function to check whether any pair exists
-# whose sum is equal to the given target value
 def twoSum(arr, target):
-  
-    # Create a set to store the elements
-    s = set()
-
-    # Iterate through each element in the array
-    for num in arr:
-      
-        # Calculate the complement that added to
-        # num, equals the target
-        complement = target - num
-
-        # Check if the complement exists in the set
-        if complement in s:
-            return True
-
-        # Add the current element to the set
-        s.add(num)
-
-    # If no pair is found
+    n = len(arr)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[k] == target:
+                return Tru
     return False
 
 print(twoSum(arr, target))
